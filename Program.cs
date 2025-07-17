91f9250f42a3d304ea1e1302cc4734d3a4262e14
alves1503 <henriquefdalves15@gmail.com>
2025-07-16 22:05:16 -0300

    Finalizei o projeto, ajustei um bug e alterei o .net para 8.0

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
 //Chamando o iphone
-Iphone iphone1 = new Iphone(numero: "1234", modelo: "Teste", imei: "1234", memoria: 1234);
+Iphone iphone1 = new Iphone(numero: "1234", modelo: "Modelo1", imei: "000000", memoria: 64);
 Console.WriteLine("Smartphone Iphone:");
 iphone1.ReceberLigacao();
 Console.WriteLine("Digite o app que deseja baixar no Iphone:");
@@ -13,7 +13,7 @@ iphone1.InstalarAplicativo(Console.ReadLine());
 Console.WriteLine("\n");
 
 //Chamando o Nokia
-Nokia nokia1 = new Nokia(numero: "1234", modelo: "Teste", imei: "1234", memoria: 1234);
+Nokia nokia1 = new Nokia(numero: "1212", modelo: "Modelo2", imei: "111111", memoria: 128);
 Console.WriteLine("Smartphone Nokia:");
 nokia1.ReceberLigacao();
 Console.WriteLine("Digite o app que deseja baixar no Nokia:");
