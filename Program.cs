4bab089fd6a511eb5ec77ddb64f10659af8c0016
alves1503 <henriquefdalves15@gmail.com>
2025-07-16 21:28:24 -0300

    Finalizei com todos os metodos e polimorfismo a classe Iphone

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
 ﻿using System.Diagnostics;
+using System.Security.Authentication;
 using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+//Chamando o iphone
+Iphone iphone1 = new Iphone(numero: "1234", modelo: "Teste", imei: "1234", memoria: 1234);
+Console.WriteLine("Smartphone Iphone:");
+iphone1.ReceberLigacao();
+iphone1.InstalarAplicativo("Zap Zap");
+
+Console.WriteLine("\n");
