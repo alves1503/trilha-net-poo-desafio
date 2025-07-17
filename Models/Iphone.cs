e8130d39ec35897dd1d062363cb736e5598cbff4
alves1503 <henriquefdalves15@gmail.com>
2025-07-16 21:34:30 -0300

    Finalizei com todos os metodos e polimorfismo a classe Nokia

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -10,7 +10,7 @@ namespace DesafioPOO.Models
         // TODO: Sobrescrever o método "InstalarAplicativo"
         public override void InstalarAplicativo(string nomeApp)
         {
-            Console.WriteLine($"Intalando o {nomeApp} ...");
+            Console.WriteLine($"Intalando o {nomeApp} no Iphone ...");
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,6 +7,12 @@ using DesafioPOO.Models;
 Iphone iphone1 = new Iphone(numero: "1234", modelo: "Teste", imei: "1234", memoria: 1234);
 Console.WriteLine("Smartphone Iphone:");
 iphone1.ReceberLigacao();
-iphone1.InstalarAplicativo("Zap Zap");
+iphone1.InstalarAplicativo("Nubank");
 
 Console.WriteLine("\n");
+
+//Chamando o Nokia
+Nokia nokia1 = new Nokia(numero: "1234", modelo: "Teste", imei: "1234", memoria: 1234);
+Console.WriteLine("Smartphone Nokia:");
+nokia1.ReceberLigacao();
+nokia1.InstalarAplicativo("Instagram");
